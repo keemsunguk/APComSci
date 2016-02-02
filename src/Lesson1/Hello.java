153824903cf1a56b6a159b355bd3c559aedf167d
Dr. S. Keem <keem@keem.net>
2016-02-02 12:09:16 -0500

    For Loop Example

--- src/Lesson1/Hello.java
+++ src/Lesson1/Hello.java
@@ -8,6 +8,7 @@ public class Hello {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello!");
-		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
 	}
 }
--- src/Shapes/Triangle.java
+++ src/Shapes/Triangle.java
@@ -19,8 +19,29 @@ public class Triangle {
 	public boolean IsValid() {
 		boolean isTriangle = false;
 		
-		// put your logic here
+		int sum1=0;
+		int sum2=0;
+		int sum3=0;
 		
+		sum1=(int)(a+b);
+		sum2=(int)(b+c);
+		sum3=(int)(c+a);
+
+		
+		if (sum1>c && sum2>a && sum3>b)
+		{
+			isTriangle = true;
+		}
+		else
+		{
+			isTriangle = false;
+
+		}
 		return isTriangle;
+	}
+	
+	public void AddSum() {
+		int sum4 = (int)(a + b);
+		
 	}
 }
